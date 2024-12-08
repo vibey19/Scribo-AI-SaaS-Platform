a40389f3130b0081546f32088522c42d862a896b
Jainil Dadhaniya <jdadhaniya6@gmail.com>
2024-12-08 17:40:04 +0530

    fixed error message

--- app/(landing)/error.tsx
+++ app/(landing)/error.tsx
@@ -3,9 +3,7 @@
 import { Empty } from "@/components/ui/empty";
 
 const Error = () => {
-  return ( 
-    <Empty label="Something went wrong." />
-   );
-}
- 
+  return <Empty label="😥 API Expired!!" />;
+};
+
 export default Error;
